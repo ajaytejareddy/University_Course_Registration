15290e64cb9f71161523c757709d71715e0a2db4
ajaytejareddy <ajaytejareddy13@gmail.com>
2020-11-08 15:37:29 -0600

    added Sign In and Sign Up files along with php action files

--- index.php
+++ index.php
@@ -134,10 +134,10 @@ hr {
     <div class="container">
       <h1 style="display:block; text-align:center;">Sign In</h1>
       <hr>
-      <label for="email"><b>Email</b></label>
+      <label for="email"><strong>Email</strong></label>
       <input type="text" placeholder="Enter Email" name="email" required>
 
-      <label for="psw"><b>Password</b></label>
+      <label for="psw"><strong>Password</strong></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
       
       <label>
@@ -156,18 +156,18 @@ hr {
 
 <div id="id01" class="modal">
   <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-  <form class="modal-content" method="$_POST" action="/action_page.php">
+  <form class="modal-content" method="$_POST" action="/SignUp.php">
     <div class="container">
       <h1>Sign Up</h1>
       <p>Please fill in this form to create an account.</p>
       <hr>
-      <label for="email"><b>Email</b></label>
+      <label for="email"><strong>Email</strong></label>
       <input type="text" placeholder="Enter Email" name="email" required>
 
-      <label for="psw"><b>Password</b></label>
+      <label for="psw"><strong>Password</strong></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
 
-      <label for="psw-repeat"><b>Repeat Password</b></label>
+      <label for="psw-repeat"><strong>Repeat Password</strong ></label>
       <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
       
       <label>
